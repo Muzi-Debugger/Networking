eae138a9ca21c798b28fca9752009312a5d64591
Muzi Mzolo <58559955+Muzi-Debugger@users.noreply.github.com>
2020-05-15 21:40:39 +0200

    Add files via upload
    Echo Server application that sends and receives data

---
+++ EchoServer Application/ReceiveUDPPacket.java
@@ -0,0 +1,23 @@
+package networking;
+/**
+ * @author Muzi Mzolo
+ */
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ReceiveUDPPacket {
+    private static final int BUFLEN = 2000;
+
+    public static void main(String[] args) throws IOException {
+
+        DatagramSocket serverSocket = new DatagramSocket(1024);
+        byte[] receiveData = new byte[BUFLEN];
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        serverSocket.receive(receivePacket);
+        String sentence = new String(receivePacket.getData());
+        System.out.println("RECEIVED: '" + sentence + "'");
+    }
+}
---
+++ EchoServer Application/SendUDPPacket.java
@@ -0,0 +1,22 @@
+package networking;
+/**
+ * @author Muzi Mzolo
+ */
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class SendUDPPacket {
+
+    private static final String MESSAGE = "Hello, World!";
+
+    public static void main(String[] args) throws IOException {
+        byte[] buffer = MESSAGE.getBytes();
+        InetAddress address = InetAddress.getLocalHost();
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 1024);
+        DatagramSocket datagramSocket = new DatagramSocket();
+        datagramSocket.send(packet);
+        System.out.println("SENT: '" + MESSAGE + "'");
+    }
+}
